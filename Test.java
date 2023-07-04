ae55e1356bcc8d529c47c1c71a8a750f325bc11b
bobrossiotta <137832556+bobrossiotta@users.noreply.github.com>
2023-07-04 13:11:19 +0300

    added all files

---
+++ Test.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        /**
+         * by bobrossiotta
+         */
+    // integer
+    //  Circle Area Calculator
+    //
+        int radius, 𝛼;
+        double 𝜋=3.14;
+        Scanner bob = new Scanner(System.in);
+
+        System.out.print("What is the value of radius?");
+        radius = bob.nextInt();
+
+        System.out.print("What is the value of measure of the central angle ?");
+        𝛼 = bob.nextInt();
+
+        double formula_area = 
+        price=(price<20)? 20 : price;
+        System.out.println("taxi fare you will pay:"+ price);
+
+
+
+    } }
